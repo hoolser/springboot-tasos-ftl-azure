0ed4bebdf5a03fdaca9e64fe412339deaf8d09bd
atsou <anastasios.tsoukas@eurodyn.com>
2025-06-22 19:55:50 +0300

    Add temporary string log to investigate issue with connectionString.

--- src/main/java/com/tasos/demo/logging/AzureBlobAppender.java
+++ src/main/java/com/tasos/demo/logging/AzureBlobAppender.java
@@ -7,6 +7,8 @@ import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -22,6 +24,7 @@ public class AzureBlobAppender extends AppenderBase<ILoggingEvent> {
     private Layout<ILoggingEvent> layout;
     private BlobContainerClient containerClient;
     private ByteArrayOutputStream logBuffer = new ByteArrayOutputStream();
+    private static final Logger logger = LoggerFactory.getLogger(AzureBlobAppender.class);
 
     public void setConnectionString(String connectionString) {
         this.connectionString = connectionString;
@@ -47,6 +50,7 @@ public class AzureBlobAppender extends AppenderBase<ILoggingEvent> {
         }
 
         try {
+            logger.info("connectionString: "+connectionString);
             BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
                     .connectionString(connectionString)
                     .buildClient();
