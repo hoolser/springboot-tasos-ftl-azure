4febfd11827330b23fd179f7405851517c21aaa7
atsou <anastasios.tsoukas@eurodyn.com>
2025-06-23 17:27:46 +0300

    Revert "Add temporary code to investigate issue with connectionString at prod."
    This reverts commit aebbad06f58d104de6fca8e893e1c3a65b7d3c1a.

--- src/main/java/com/tasos/demo/TasosApplication.java
+++ src/main/java/com/tasos/demo/TasosApplication.java
@@ -4,17 +4,12 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-
 
 @SpringBootApplication
 public class TasosApplication {
 
     @Value("${azure-storage-connection-string:not-set}")
     private String azureStorageConnectionStringFromPropertiesOrVault;
-
-    @org.springframework.beans.factory.annotation.Autowired
-    private Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(TasosApplication.class, args);
@@ -25,9 +20,7 @@ public class TasosApplication {
         // This method is called after the application context is initialized
         // You can perform any initialization logic here if needed
         System.out.println("TasosApplication has been initialized successfully.");
-
-        System.out.println("Active Spring Profiles: " + String.join(", ", environment.getActiveProfiles()));
+        System.out.println("Active Spring Profiles: " + System.getProperty("spring.profiles.active"));
         System.out.println("azureStorageConnectionStringFromPropertiesOrVault: " + azureStorageConnectionStringFromPropertiesOrVault);
-
     }
 }
