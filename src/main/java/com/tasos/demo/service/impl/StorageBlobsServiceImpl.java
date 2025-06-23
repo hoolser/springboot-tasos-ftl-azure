e69275ab1d649fc98a7bb760118ac7f23e93758c
atsou <anastasios.tsoukas@eurodyn.com>
2025-06-23 17:22:42 +0300

    Revert "Add temporary code to investigate issue with connectionString at prod."
    This reverts commit 5caf62e4103cd50d03e06537fa0c0cb99cf67751.

--- src/main/java/com/tasos/demo/service/impl/StorageBlobsServiceImpl.java
+++ src/main/java/com/tasos/demo/service/impl/StorageBlobsServiceImpl.java
@@ -19,7 +19,7 @@ public class StorageBlobsServiceImpl implements StorageBlobsService {
 
     private static final Logger logger = LoggerFactory.getLogger(StorageBlobsServiceImpl.class);
 
-    @Value("${azure-storage-connection-string-temporary}")
+    @Value("${azure-storage-connection-string}")
     private String storageConnectionString;
 
     @Override
